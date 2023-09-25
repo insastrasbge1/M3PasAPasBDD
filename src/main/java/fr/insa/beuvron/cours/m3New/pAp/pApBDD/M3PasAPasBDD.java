7dfb8b6b2f04485478b27de715e203f2a7df9347
insastrasbge1 <insastrasbge1@gmail.com>
2023-09-25 22:58:05 +0200

    config pom pour mysql connector

---
+++ src/main/java/fr/insa/beuvron/cours/m3New/pAp/pApBDD/M3PasAPasBDD.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.beuvron.cours.m3New.pAp.pApBDD;
+
+/**
+ *
+ * @author francois
+ */
+public class M3PasAPasBDD {
+
+
+}
